a2a41b915f202368a7dc79526fbb92ea90a39218
juanaugusto <juan.augusto@ufrj.br>
2017-05-09 12:17:41 -0300

    Changing Calculate Palindrome code to see "graph nodes"

--- src/main/java/br/ufrj/testproj/principal/CalculatePalindromeOBI.java
+++ src/main/java/br/ufrj/testproj/principal/CalculatePalindromeOBI.java
@@ -7,6 +7,7 @@ public class CalculatePalindromeOBI {
 
 	public static String calculateWrongPalindrome(char[] letras) {
 		//Numero 1
+		System.out.print("1 ");
 		noop();
 		String retorno;
 		int contador_texto, i, j;
@@ -17,53 +18,67 @@ public class CalculatePalindromeOBI {
 		noop();
 		
 		//Numero 2
+		System.out.print("2 ");
 		for (char letra : letras) {
 			
 			//Numero 3
+			System.out.print("3 ");
 			if (letra != ' ') {
 				
 				//Numero 4
+				System.out.print("4 ");
 				texto[contador_texto] = letra;
 				contador_texto++;
 			}
 			
 			//Numero 5
+			System.out.print("5 ");
 			noop();
 			noop();
 		}
 		
 		//Numero 6
+		System.out.print("6 ");
 		i = 0;
 		j = contador_texto - 1;
 		palindromo = 1;
 		
 		//Numero 7
+		System.out.print("7 ");
 		while (j > i) {
 			
 			//Numero 8
+			System.out.print("8 ");
 			if (texto[i] != texto[j]) {
 			
 				//Numero 9
+				System.out.print("9 ");
 				palindromo = 0;
 			}
 			
 			//Numero 10
+			System.out.print("10 ");
 			i = i + 1;
 			j = j - 1;
 		}
 		
 		//Numero 11
+		System.out.print("11 ");
 		if (palindromo == 1) {
 			
 			//Numero 12
+			System.out.print("12 ");
 			retorno = "SIM";
 		} else {
 			
 			//Numero 13
+			System.out.print("13 ");
 			retorno = "NAO";
 		}
 		
 		//Numero 14
+		System.out.print("14 ");
+		System.out.println();
 		return retorno;
 	}
 
