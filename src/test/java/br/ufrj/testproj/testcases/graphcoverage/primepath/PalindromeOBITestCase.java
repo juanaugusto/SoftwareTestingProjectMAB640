d5e9eecf03233973b51e6e4d823308869a7c481f
juanaugusto <juan.augusto@ufrj.br>
2017-05-11 15:35:04 -0300

    Changing palindrome primepath tests

--- src/test/java/br/ufrj/testproj/testcases/graphcoverage/primepath/PalindromeOBITestCase.java
+++ src/test/java/br/ufrj/testproj/testcases/graphcoverage/primepath/PalindromeOBITestCase.java
@@ -3,7 +3,9 @@ package br.ufrj.testproj.testcases.graphcoverage.primepath;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.ufrj.testproj.principal.CalculatePalindromeOBI;
@@ -14,10 +16,35 @@ public class PalindromeOBITestCase {
 	String expectedResponse;
 	int[] percurredPath;
 	
+	int[] nodeRequirements;
+	int[][] edgeRequirements;
+	int[][] edgePairRequirements;
 	int[][] primePathsRequirements;
+	
+	@BeforeClass
+	public static void setUpOnceForAll(){
+		System.out.println("Starting tests");
+		
+	}
+	
+	@AfterClass
+	public static void tearDownOnceForAll(){
+		System.out.println("Finishing tests");
+	}
 
 	@Before
 	public void setUp(){
+		this.nodeRequirements = new int [] { 1,2,3,4,5,6,7,8,9,10,11,12,13,14 };
+		this.edgeRequirements = new int [][] { 
+			{1, 2}, {2, 3}, {2, 6}, {3, 4}, {3, 5}, {4, 5}, {5, 2}, {6, 7}, 
+			{7, 8}, {7, 11}, {8, 9}, {8, 10}, {9, 10}, {10, 7}, {11, 12}, {11, 13},
+			{12, 14}, {13, 14}
+		};
+		this.edgePairRequirements = new int [][] {
+			{1, 2, 3}, {1, 2, 6}, {2, 3, 4}, {2, 3, 5}, {2, 6, 7}, {3, 5, 2}, 
+			{3, 4, 5}, {4, 5, 2}, {5, 2, 3}, {5, 2, 6}, {6, 7, 8}, {6, 7, 11}, 
+			{7, 8, 9}, {7, 8, 10},  {7, 11, 12}, {7, 11, 13}, {8, 9, 10}, {8, 10, 7}, 
+			{9, 10, 7}, {10, 7, 8}, {10, 7, 11}, {11, 12, 14}, {11, 13, 14}};
 		this.primePathsRequirements = new int[][] {
 			{3,4,5,2,6,7,8,9,10},  {3,4,5,2,6,7,11,12,14}, 
 			{3,4,5,2,6,7,11,13,14}, {3,5,2,6,7,8,9,10}, {3,4,5,2,6,7,8,10}, 
@@ -28,7 +55,9 @@ public class PalindromeOBITestCase {
 			{4,5,2,3,4}, {5,2,3,4,5}, {1,2,3,4,5}, {2,3,4,5,2}, 
 			{9,10,7,8,9}, {10,7,8,9,10}, {7,8,9,10,7},  {8,9,10,7,8},  
 			{3,5,2,3}, {5,2,3,5}, {1,2,3,5}, {2,3,5,2}, {10,7,8,10}, 
-			{8,10,7,8}, {7,8,10,7}};
+			{8,10,7,8}, {7,8,10,7}
+		};
+		
 	}
 	
 	@After
