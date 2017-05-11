fd3ae7c4e1e3963ccdd6826a58d43c1d967ab3b6
juanaugusto <juan.augusto@ufrj.br>
2017-05-11 09:35:30 -0300

    Addink Bank Test for Node and changing Calculate Bank OBI to print results

--- src/main/java/br/ufrj/testproj/principal/CalculateBankOBI.java
+++ src/main/java/br/ufrj/testproj/principal/CalculateBankOBI.java
@@ -7,46 +7,91 @@ import java.util.List;
 public class CalculateBankOBI {
 
 	public static int calculateWrongBank(int C, int N, int[] T, int[] D) {
-		
+
 		int[][] tempos = new int[T.length][2];
 
-		for (int i = 0; i < T.length; i++){
+		for (int i = 0; i < T.length; i++) {
 			tempos[i][0] = T[i];
 			tempos[i][1] = D[i];
 		}
 		
+		System.out.print("1 ");
+		// Number 1
 		if (N <= C) {
+
+			System.out.print("2 ");
+			System.out.println();
+			// Number 2
 			return 0;
-		} else {
+		}
+		
+		else {
+			System.out.print("3 ");
+			// Number 3 - else
+		
 			int cont = 0;
 			List<Integer> termina = new ArrayList<Integer>();
 			termina.add(tempos[0][0] + tempos[0][1]);
 			int caixas = C - 1;
-
+			
+			System.out.print("4 ");
+			// Number 4
 			for (int i = 1; i < N; i++) {
 				
+				System.out.print("6 ");
+				// Number 6
 				Collections.sort(termina);
+				
+				System.out.print("7 ");
+				// Number 7
 				while (termina.get(0) <= tempos[i][0]) {
+					
+					System.out.print("8 ");
+					// Number 8
 					termina.remove(0);
 					caixas = caixas + 1;
 					if (termina.size() == 0) {
+						
+						System.out.print("9 ");
+						// Number 9
 						break;
 					}
 				}
 
+				System.out.print("10 ");
+				// Number 10
 				if (caixas > 1) {
+					
+					System.out.print("11 ");
+					// Number 11
 					caixas = caixas - 1;
 					termina.add(tempos[i][0] + tempos[i][1]);
 
-				} else {
+				} 
+
+				else {
 					Integer atendimento = Collections.min(termina);
 					termina.remove(atendimento);
 					termina.add(atendimento + tempos[i][1]);
+					
+					System.out.print("12 ");
+					// Number 12
 					if (atendimento - tempos[i][0] > 20) {
+						
+						System.out.print("13 ");
+						// Number 13
 						cont = cont + 1;
 					}
 				}
+				
+				System.out.print("14 ");
+				// Number 14
+				// i = i + 1 (implicito do for)
 			}
+			
+			System.out.print("5 ");
+			System.out.println();
+			// Number 5
 			return cont;
 
 		}
--- src/test/java/br/ufrj/testproj/testcases/graphcoverage/node/BankOBITest.java
+++ src/test/java/br/ufrj/testproj/testcases/graphcoverage/node/BankOBITest.java
@@ -1,5 +1,67 @@
 package br.ufrj.testproj.testcases.graphcoverage.node;
 
+import br.ufrj.testproj.principal.CalculateBankOBI;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class BankOBITest {
 
+	int C, N;
+	int[] T, D;
+	int expectNumberClientsWait;
+	
+	@Test
+	public void testPathNumber1(){
+		this.C = 1;
+		this.N = 1;
+		this.T = new int[] { 0 };
+		this.D = new int[] { 1 };
+
+		this.expectNumberClientsWait = 0;
+
+		//assertEquals(expectNumberClientsWait, CalculateBankOBI.calculateWrongBank(this.C, this.N, this.T, this.D));
+		
+	}
+	
+	@Test
+	public void testPathNumber2(){
+		//Infeasible path
+				
+	}
+	
+	@Test
+	public void testPathNumber3(){
+		//Infeasible path
+				
+	}
+
+	@Test
+	public void testPathNumber4(){
+		//Infeasible path
+		this.C = 1;
+		this.N = 2;
+		this.T = new int[] { 0, 0 };
+		this.D = new int[] { 3, 3 };
+
+		this.expectNumberClientsWait = 0;
+
+		assertEquals(expectNumberClientsWait, CalculateBankOBI.calculateWrongBank(this.C, this.N, this.T, this.D));
+		
+	}
+
+	@Test
+	public void testNoBAK() {
+
+//		this.C = 1;
+//		this.N = 1;
+//		this.T = new int[] { 0 };
+//		this.D = new int[] { 1 };
+//
+//		this.expectNumberClientsWait = 0;
+//
+//		assertEquals(expectNumberClientsWait, CalculateBankOBI.calculateWrongBank(this.C, this.N, this.T, this.D));
+	}
+
 }
+
